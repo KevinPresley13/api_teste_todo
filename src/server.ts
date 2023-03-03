360a0527d2459ddc60d3a3b606b6263948c6533d
Kevin Presley <kevinpresley1995@gmail.com>
2023-03-02 21:06:35 -0300

    criando servidor

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import  express, { Request, Response }  from "express";
+import path, { join } from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+const server = express();
+
+
+server.use(express.static(path.join(__dirname, '../public')));
+server.use(express.urlencoded({extended: true}));
+
+server.use((req: Request, res: Response)=>{
+    res.status(404);
+    res.json({erro:'pagina nao encontrada'})
+});
+
+
+
+server.listen(process.env.PORT);
